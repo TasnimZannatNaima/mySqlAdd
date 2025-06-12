14159bde961faab4f2a011ae238748f770a9e878
Tasnim Zannat Naima <naimatasnimzannat@gmail.com>
2025-06-12 16:22:12 +0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.mysqladd {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.mysqladd to javafx.fxml;
+    exports org.example.mysqladd;
+}
---
+++ src/main/java/org/example/mysqladd/BirthdayApp.java
@@ -0,0 +1,15 @@
+package org.example.mysqladd;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BirthdayApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
