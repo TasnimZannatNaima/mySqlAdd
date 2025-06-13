3bf6eee1465cdb0a07d5bc4e99c43af0705c3815
Tasnim Zannat Naima <naimatasnimzannat@gmail.com>
2025-06-13 12:47:34 +0600

    Update Database.java

--- src/main/java/org/example/mysqladd/Database.java
+++ src/main/java/org/example/mysqladd/Database.java
@@ -5,11 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/birthday";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/birthday";//naima
     private static final String USER = "root";
-    private static final String PASSWORD = "12345"; // আপনার পাসওয়ার্ড দিন
+    private static final String PASSWORD = "12345";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(URL, USER, PASSWORD);//naima
     }
 }
